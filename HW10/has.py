3eb967f11beb3e90aea6379d4d6b8780f7ced10f
DrPRun <sheighd@gmail.com>
2022-11-23 16:55:14 +0300

    purchase

---
+++ HW10/has.py
@@ -0,0 +1,33 @@
+        OLS Regression Results                            
+======================================
+Dep. Variable:                   wage
+Model:                            OLS
+Method:                 Least Squares
+Date:                Tue, 15 Nov 2022
+Time:                        21:16:09
+No. Observations:                 852
+Df Residuals:                     838
+Df Model:                          13
+Covariance Type:            nonrobust
+=====================================
+                 coef    std err     
+-------------------------------------
+Intercept  -5483.8885     90.116    -
+hours          1.6350      0.585     
+IQ             0.0576      0.339     
+KWW            1.9626      0.704     
+exper         -0.2804      1.122     
+tenure        -4.2791      0.853     
+age            0.1178      1.723     
+married      -15.9544     14.305     
+black         21.1315     14.841     
+south         21.7609      9.137     
+urban         -5.2156      9.627     
+sibs          -1.0976      2.366     
+brthord       -0.9482      3.243     
+lwage        934.6097     11.365     
+
+
+Notes:
+[1] Standard Errors assume that the covariance matrix of the errors is correctly specified.
+[2] The condition number is large, 2.73e+03. This migh
